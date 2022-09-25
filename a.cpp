ee0e8e63855905634dd9943abb42b93b35c6418a
Alam <muhammadalamsyah47@gmail.com>
2022-09-25 08:20:59 +0700

    add new template

---
+++ a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define int long long
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define all(v) v.begin(), v.end()
+
+#define trace(x) cout << '>' << #x << ':' << x << "\n"
+#define trace2(x,y) cout<< '>' << #x << ':' << x << " | " << #y << ':' << y << "\n"
+#define trace3(a,b,c) cout<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<"\n"
+#define trace4(a,b,c,d) cout<<#a<<"="<<(a)<<", "<<#b<<"="<<(b)<<", "<<#c<<"="<<(c)<<", "<<#d<<"="<<(d)<<"\n"
+
+template <typename T>
+void print(T var){
+    cout << var << ' ';
+    return;
+}
+
+template <typename T>
+void print(vector <T> var){
+    for (auto x: var)
+        print(x);
+    cout << "\n";
+    return;
+}
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+const int mod = 1e9+7;
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+}
